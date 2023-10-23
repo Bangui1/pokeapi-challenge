6c2695b3fa4c26634bd77ec551a660eb1c8e4efa
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-10-23 11:21:28 -0300

    feat: pokemon evolutions

---
+++ src/PokemonProfile/PokemonEvolutions.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import useFetchEvolutions from "./useFetchEvolutions";
+import { useNavigate } from "react-router-dom";
+
+export interface Evolution {
+    id: number;
+    name: string;
+    image: string;
+}
+
+function PokemonEvolutions({ id }: { id: number | undefined }) {
+    const [evolutions] = useFetchEvolutions(id);
+
+    const nav = useNavigate();
+   
+    return (
+        <div className='evolution-container'>
+            {evolutions.map((evolution, index) => (
+              <div key={index} className='evolution-set'>
+                {evolution.map((pokemon: Evolution) => (
+                  <div key={pokemon.id} className='evolution'>
+                    <div className='evolution-info' onClick={() => nav(`/${pokemon.id}`)}>
+                    <img className='evolution-image' src={pokemon.image} alt={pokemon.name} />
+                    <div className='evolution-name'>#{pokemon.id} {pokemon.name}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+      );
+  }
+  
+  export default PokemonEvolutions;
