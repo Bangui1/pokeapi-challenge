ec967c30414b6b2fb02f81acf84fb5baa00fba18
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-10-25 23:49:29 -0300

    Logo header in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PokemonHome from "./PokemonHome/PokemonHome";
 import PokemonProfile from "PokemonProfile/PokemonProfile";
+import Logo from "Logo/Logo";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Logo />
         <Routes>
           <Route path="/" element={<PokemonHome />} />
           <Route path="/:id" element={<PokemonProfile />} />
